95993ff94147413784ac94bc0dd24251595a7647
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-06-24 09:35:12 +0300

    tailwindcss integration as in https://bit.ly/2M7lbSM

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { guessProductionMode } = require('@ngneat/tailwind');
+
+module.exports = {
+  prefix: '',
+  purge: {
+    enabled: guessProductionMode(),
+    content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
