826fa466c4ff2e01b78841f187091403cbbd1a10
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-06-25 19:33:34 +0300

    AngularFireModule

--- apps/gnosys/src/app/app.module.ts
+++ apps/gnosys/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
