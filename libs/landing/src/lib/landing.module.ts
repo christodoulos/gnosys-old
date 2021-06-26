a66821f20c3461a7205111ea7fa072e79bdc3cb4
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-06-26 08:55:01 +0300

    landing routing and component

--- libs/landing/src/lib/landing.module.ts
+++ libs/landing/src/lib/landing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingRoutingModule } from './landing.routing';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LandingRoutingModule],
+  declarations: [LandingComponent],
+  exports: [LandingComponent],
 })
 export class LandingModule {}
