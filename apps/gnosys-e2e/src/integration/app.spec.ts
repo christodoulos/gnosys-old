7e417d9aaa5992aaa9655dbd899c2fefcc353be6
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-06-27 12:31:29 +0300

    e2e test fix

--- apps/gnosys-e2e/src/integration/app.spec.ts
+++ apps/gnosys-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('gnosys', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to gnosys!');
+    getGreeting().contains('Ready to dive in?');
   });
 });
