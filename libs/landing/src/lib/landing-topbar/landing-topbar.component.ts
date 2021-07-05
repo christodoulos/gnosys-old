ad464b1fde6d79a6b6d9cff343c81907e716e26e
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-05 10:35:47 +0300

    lint fixes

--- libs/landing/src/lib/landing-topbar/landing-topbar.component.ts
+++ libs/landing/src/lib/landing-topbar/landing-topbar.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'gnosys-landing-topbar',
   templateUrl: './landing-topbar.component.html',
   styleUrls: ['./landing-topbar.component.css'],
 })
-export class LandingTopbarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class LandingTopbarComponent {}
