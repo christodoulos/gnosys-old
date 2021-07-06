d5fbbeb41401d2a058fbbdd43723a355d426b1ae
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-06 18:11:41 +0300

    run unsubscribes immediately after brython runs

--- libs/vendor/src/lib/brython-runner/brython-runner.component.ts
+++ libs/vendor/src/lib/brython-runner/brython-runner.component.ts
@@ -1,13 +1,11 @@
 import {
   Component,
-  OnInit,
   ChangeDetectionStrategy,
   ViewChild,
   ElementRef,
   AfterViewInit,
 } from '@angular/core';
 import { CodemirrorQuery } from '@gnosys/state';
-import { take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'gnosys-brython-runner',
@@ -15,14 +13,12 @@ import { take, tap } from 'rxjs/operators';
   styleUrls: ['./brython-runner.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrythonRunnerComponent implements OnInit, AfterViewInit {
+export class BrythonRunnerComponent implements AfterViewInit {
   @ViewChild('brython_runner') iFrameElmRef: ElementRef | undefined;
   currcode$ = this.codemirrorQuery.currcode$;
   iframe: HTMLIFrameElement | undefined;
   iWindow: Window | null | undefined;
   constructor(private codemirrorQuery: CodemirrorQuery) {}
-
-  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     if (this.iFrameElmRef) this.iframe = this.iFrameElmRef.nativeElement;
@@ -30,9 +26,11 @@ export class BrythonRunnerComponent implements OnInit, AfterViewInit {
   }
 
   run(): void {
-    this.currcode$.subscribe((value) => {
-      console.log(value);
-      this.iWindow?.postMessage(value, this.iWindow.location.href);
-    });
+    this.currcode$
+      .subscribe((value) => {
+        console.log(value);
+        this.iWindow?.postMessage(value, this.iWindow.location.href);
+      })
+      .unsubscribe();
   }
 }
